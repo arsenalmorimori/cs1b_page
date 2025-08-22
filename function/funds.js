9579c74576b48f6383a9931c4c3b960623c86ee3
arsenalmorimori <arsenalbautista1@gmail.com>
2025-08-22 13:05:29 +0800

    Add and View (finished)

--- function/funds.js
+++ function/funds.js
@@ -4,13 +4,30 @@ const supabaseUrl = url
 const supabaseKey = key
 const client = supabase.createClient(supabaseUrl, supabaseKey)
 
+
+// LIST & SEARCHBOX
 list = document.getElementById("list")
 search_box = document.getElementsByClassName("search_box")[0]
 
+
+// DATABASE STORAGE
+const id_array = []
 const name_array = []
 const total_contribution_array = []
+let name_array_len = 0;
+
+
+// POPUP COMPONENT
+const popup_container = document.getElementsByClassName("popup_container")[0]
+const add_box = document.getElementsByClassName("add_box")[0]
+const view_box = document.getElementsByClassName("view_box")[0]
+const close_add = document.getElementsByClassName("close")[0]
+const close_view = document.getElementsByClassName("close")[1]
+        
+
+
+// MAIN / READ METHOD
 async function read(){
-    
     
     let { data, error } = await client
     .from("funds_table")
@@ -22,8 +39,268 @@ async function read(){
         console.log(data)
         list.innerHTML = ``;
         for(let a = 0; a < data.length ; a++){
+            id_array.push(data[a].id)
             name_array.push(data[a].name)
+            name_array_len = data.length
             total_contribution_array.push(data[a].total_contribution)
+            list.innerHTML += `
+            <div class="column_wrapper">
+                <button class="name_btn" value=${a}>
+                    <div>
+                        <span>${a}</span>
+                        <span>${name_array[a]}</span>
+                        <span> ${parseFloat(total_contribution_array[a]).toFixed(2)}</span>
+                    </div>    
+                </button>
+
+                <div>
+                    <button class="add_btn"  value=${a}>Add</button>
+                    <button class="view_btn" value=${a}>View</button>
+                </div>
+            </div>`
+
+        }   
+
+
+        // if ADD_BTN isPRESS
+        add_btn = document.getElementsByClassName("add_btn")
+     
+        for(let b = 0 ; b < name_array_len ; b++){
+            add_btn[b].addEventListener("click", function(){
+                console.log(this.value)
+                add_function(b)
+                add_button_pressed(b)
+
+            })
+        }
+
+
+        // if VIEW_BTN isPRESS
+        view_btn = document.getElementsByClassName("view_btn")
+
+        for(let c = 0 ; c < name_array_len ; c++){
+            view_btn[c].addEventListener("click", function(){
+                console.log(this.value)
+                view_function(c)
+            })
+        }
+
+
+        // FOR MOBILE INTERACTION
+
+        name_btn = document.getElementsByClassName("name_btn")
+        for(let d = 0 ; d < name_array.length ; d ++){
+            name_btn[d].addEventListener("click", function(){
+                console.log(this.value)
+                add_function(d)
+            })
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+// ADD POPUP FUNCTIONS
+
+add = document.getElementsByClassName("add")[0]
+amount = document.getElementsByClassName("amount")[0]
+description = document.getElementsByClassName("description")[0]
+
+async function add_function(index){
+    openAddBox()
+    displayName(index)
+
+
+    add.addEventListener("click", function(){
+    if(amount.value == ""){
+            alert("empty amount input")
+            return;
+        }
+        if(description.value == ""){
+            alert("empty decription input")
+            return;
+        }
+        
+        // add_contribution()
+        console.log("--------------------------")
+        console.log(id_array[index])
+        console.log(name_array[index])
+        console.log(total_contribution_array[index])
+        console.log("--------------------------")
+
+        add_contribution(index,amount.value,description.value)
+    }) 
+
+}
+
+// -- INSERT to DB
+async function add_contribution(index, amount_input,description_input) {
+    const { data, error } = await client
+    .from('view_table')
+    .insert([
+      { fk_id: id_array[index], 
+          isAdd: 'true',
+          amount: amount_input,
+          description : description_input},
+    ])
+    .select()
+
+    if(error){
+      console.log(error)
+    }else{
+    //   console.log("added") 
+        let updated_contribution =
+        parseFloat(total_contribution_array[index]) + parseFloat(amount.value)
+        update_total_contribution(index, updated_contribution)
+    }
+}
+
+// -- UPDATE DB
+async function update_total_contribution(index, amount_input) {
+    const { data, error } = await client
+    .from('funds_table')
+    .update({ total_contribution: amount_input })
+    .eq('id', id_array[index])
+    .select()
+
+    if(error){
+      console.log(error)
+    }else{
+    //   console.log("updated")
+    alert("Done")
+    location.reload() 
+    }
+}
+
+// -- display name only
+name_title = document.getElementsByClassName("title")[0]
+function displayName(index){
+    name_title.innerHTML = name_array[index]
+}
+
+// -- open and close
+function openAddBox(){
+    popup_container.classList.toggle("show");
+    add_box.classList.toggle("show");
+}
+
+close_add.addEventListener("click", function(){
+    popup_container.classList.toggle("show");
+    add_box.classList.toggle("show");
+})
+
+
+
+
+
+
+
+
+
+
+// VIEW POPUP FUNCTIONS
+
+async function view_function(index) {
+    display_name_contribution(index)
+    openViewBox()
+    read_view_table(index)
+} 
+
+
+
+
+
+// GET and display view_table
+view_list = document.getElementById("view_list")
+async function read_view_table(index) {
+    view_list.innerHTML = ``
+  
+    
+    
+    let { data, error } = await client
+    .from('view_table')
+    .select('*').eq('fk_id', id_array[index])
+
+
+
+    if(error){
+        console.log(error)
+    }else{
+        console.log(data)
+        data.forEach(element => {
+            view_list.innerHTML += `
+            <div>
+                <span>${parseFloat(element.amount).toFixed(2)}</span>
+                <span>${element.description}</span>
+                <span>
+                
+                ${format_date(element.created_at)}
+                
+                </span>
+            </div>`
+        });
+    }
+    
+} 
+
+// -- display name and total_contribution only
+view_name = document.getElementsByClassName("view_name")[0]
+view_amount = document.getElementsByClassName("view_amount")[0]
+
+function display_name_contribution(index){
+    view_name.innerHTML = name_array[index]
+    view_amount.innerHTML = total_contribution_array[index]
+}
+
+// -- date formatter
+function format_date(isoString){
+    // convert to Date object
+    const date = new Date(isoString);
+
+    // extract parts
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // months start at 0
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
+    // format mm/dd. hh:mm
+    const formatted = `${month}/${day}. ${hours}:${minutes}`;
+    return formatted;
+}
+
+// -- to open and close
+function openViewBox(index){
+    popup_container.classList.toggle("show");
+    view_box.classList.toggle("show");
+}
+
+close_view.addEventListener("click", function(){
+    popup_container.classList.toggle("show");
+    view_box.classList.toggle("show");
+})
+
+
+ 
+
+
+
+
+// SEARCH FUNCTION
+search_box.addEventListener("keyup", function search(){
+    console.log(search_box.value)
+    let isEmpty = true;
+    list.innerHTML = ``
+    for(let a = 0 ; a < name_array.length ; a++){
+        if(name_array[a].toLowerCase().includes(search_box.value.toLowerCase())){
             list.innerHTML += `
             <div class="column_wrapper">
                 <button onclick="console.log(this.value)" class="name_btn" value=${a}>
@@ -35,35 +312,10 @@ async function read(){
                 </button>
 
                 <div>
-                    <button class="add_btn" onclick="click_add(this.value)" value=${a}>Add</button>
-                    <button class="view_btn" onclick="click_view(this.value)" value=${a}>View</button>
+                    <button class="add_btn"  value=${a}>Add</button>
+                    <button class="view_btn" value=${a}>View</button>
                 </div>
             </div>`
-
-        }
-    }
-
-}
-
-function click_add(val){
-    console.log("add " + val)
-}
-
-function click_view(val){
-    console.log("view " + val)
-}
-
-
-       
-
-
-search_box.addEventListener("keyup", function search(){
-    console.log(search_box.value)
-    let isEmpty = true;
-    list.innerHTML = ``
-    for(let a = 0 ; a < list_array.length ; a++){
-        if(list_array[a].toLowerCase().includes(search_box.value.toLowerCase())){
-            list.innerHTML += `<buttton value=${a}><li>${name_array[a]} ${total_contribution_array[a]} </li>`
             isEmpty = false;
         }
     }
@@ -74,4 +326,10 @@ search_box.addEventListener("keyup", function search(){
 
 })
 
+
+
+
+// Call READ or MAIN Method
 read()
+
+
