7af8b5227e01c019932ddc0a751dd3de0492c43c
arsenalmorimori <arsenalbautista1@gmail.com>
2025-08-24 12:23:39 +0800

    API keys

---
+++ env.js
@@ -0,0 +1,5 @@
+const url = process.env.url
+const key = process.env.key
+const receipt_webhook = process.env.receipt_webhook 
+const announece_webhook = process.env.announece_webhook
+const savings_webhook = process.env.savings_webhook
